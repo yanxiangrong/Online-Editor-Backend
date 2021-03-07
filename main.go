543d958369a45d65eed6d1012d16abb131af4e5c
yanxiangrong <yanxiangrong_2@outlook.com>
2021-03-07 16:54:13 +0800

    增加跨域中间件

--- main.go
+++ main.go
@@ -157,20 +157,20 @@ func PathExists(path string) (bool, error) {
 }
 
 func initDBConfig() DatabaseConfig {
-	config := DatabaseConfig{
-		os.Getenv("DB_USERNAME"),
-		os.Getenv("DB_PASSWD"),
-		os.Getenv("DB_ADDR"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_DBNAME"),
-	}
 	//config := DatabaseConfig{
-	//	"OnlineEditor",
-	//	"password",
-	//	"yandage.top",
-	//	"3306",
-	//	"onlineeditor",
+	//	os.Getenv("DB_USERNAME"),
+	//	os.Getenv("DB_PASSWD"),
+	//	os.Getenv("DB_ADDR"),
+	//	os.Getenv("DB_PORT"),
+	//	os.Getenv("DB_DBNAME"),
 	//}
+	config := DatabaseConfig{
+		"OnlineEditor",
+		"password",
+		"yandage.top",
+		"3306",
+		"onlineeditor",
+	}
 	log.Println(config.ToString())
 	return config
 }
@@ -665,33 +665,19 @@ func GetRunUser() RunUser {
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
-		origin := c.Request.Header.Get("Origin") //请求头部
+		origin := c.Request.Header.Get("Origin")
 		if origin != "" {
-			//接收客户端发送的origin （重要！）
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-			//服务器支持的所有跨域请求的方法
-			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
-			//允许跨域设置可以返回其他子段，可以自定义字段
-			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session")
-			// 允许浏览器（客户端）可以解析的头部 （重要）
-			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
-			//设置缓存时间
-			c.Header("Access-Control-Max-Age", "172800")
-			//允许客户端传递校验信息比如 cookie (重要)
-			c.Header("Access-Control-Allow-Credentials", "true")
+			c.Header("Access-Control-Allow-Origin", origin)
+			//主要设置Access-Control-Allow-Origin
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "false")
+			c.Set("content-type", "application/json")
 		}
-
-		//允许类型校验
 		if method == "OPTIONS" {
-			c.Status(http.StatusOK)
+			c.AbortWithStatus(http.StatusNoContent)
 		}
-
-		defer func() {
-			if err := recover(); err != nil {
-				log.Printf("Panic info is: %v", err)
-			}
-		}()
-
 		c.Next()
 	}
 }
